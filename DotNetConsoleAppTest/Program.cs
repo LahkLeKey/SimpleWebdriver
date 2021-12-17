323dc9e34909d4c7a6be5c73a34a3850e02aaf94
LahkLeKey <53668012+LahkLeKey@users.noreply.github.com>
2021-12-16 18:11:46 -0600

    Add .gitignore

--- DotNetConsoleAppTest/Program.cs
+++ DotNetConsoleAppTest/Program.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-
-namespace DotNetConsoleAppTest
+﻿namespace DotNetConsoleAppTest
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
     {
         [DllImport("kernel32.dll", ExactSpelling = true)]
         private static extern IntPtr GetConsoleWindow();
@@ -21,91 +22,92 @@ namespace DotNetConsoleAppTest
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool SetForegroundWindow(IntPtr hWnd);
 
-        private static void BringConsoleToFront()
+        /// <summary>
+        /// The bring console to front.
+        /// </summary>
+        private static void BringConsoleToFront() => SetForegroundWindow(GetConsoleWindow());
+
+        /// <summary>
+        /// The driver executable path.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        private static string DriverExecutablePath()
         {
-            SetForegroundWindow(GetConsoleWindow());
+            var executablePath = Assembly.GetExecutingAssembly().Location;
+            var driverExecutablePath = $"{Path.GetDirectoryName(executablePath)}";
+            Print($"Web Driver Path : {driverExecutablePath}");
+            return driverExecutablePath;
         }
 
-        private static void Print(object message)
+        /// <summary>
+        /// The main loop.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
         {
-            BringConsoleToFront();
-            Console.WriteLine(String.Concat(Enumerable.Repeat("-", 96)));
-            Console.WriteLine(message.ToString());
-            Console.WriteLine(String.Concat(Enumerable.Repeat("-", 96)));
-        }
+            Print(
+                $"Error handling has not been implemented for this program.\nThis has been done as a quick and dirty refresher for both C# and Selenium.\n");
 
-        static void Main(string[] args)
-        {
-            Print($"Error handling has not been implemented for this program.\nThis has been done as a quick and dirty refresher for both C# and Selenium.\n");
+            var driverExecutablePath = DriverExecutablePath();
 
-            string assemblyExecutablePath = Assembly.GetExecutingAssembly().Location;
-            string webDriverExecutablePath = $"{Path.GetDirectoryName(assemblyExecutablePath)}";
-            Print($"Web Driver Path : {webDriverExecutablePath}");
-
-            IList<string> validGeoCodes = new List<string>();
-
-            ChromeOptions options = new ChromeOptions();
+            var options = new ChromeOptions();
             options.AddArgument("--kiosk"); // Start in fullscreen since we don't care about mobile support for now.
             options.AddArgument("--log-level=3"); // Silence driver logging.
-            IWebDriver driver = new ChromeDriver(webDriverExecutablePath, options);
+            IWebDriver driver = new ChromeDriver(driverExecutablePath, options);
             driver.Url = "https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes";
             Print($"Driver Object : {driver}");
 
-
-            By xPathSelector = By.XPath("//a[@title='ISO 3166-1 alpha-2']");
+            var xPathSelector = By.XPath("//a[@title='ISO 3166-1 alpha-2']");
             Print($"Parsing DOM for target elements please wait...");
             IReadOnlyCollection<IWebElement> alpha2Codes = driver.FindElements(xPathSelector);
 
             Print($"Parsing targeted elements please wait...");
-            foreach (IWebElement EGeoCode in alpha2Codes)
-            {   
-                // We are using Alpha-2 Codes. Everything else should be ignored.
-                if (EGeoCode.Text.Length == 2)
-                {
-                    validGeoCodes.Add(EGeoCode.Text);
-                }
+            var validGeoCodes = (from eGeoCode in alpha2Codes where eGeoCode.Text.Length == 2 select eGeoCode.Text)
+                .ToList();
+
+            if (validGeoCodes.Count != 249)
+            {
+                Print(
+                    $"Warning, Something went wrong when parsing geo codes. We should have 249 total and we have {validGeoCodes.Count}");
             }
-
-
-            int foundGeoCount = validGeoCodes.Count;
-            if (foundGeoCount != 249)
-                Print($"Warning, Something went wrong when parsing geo codes. We should have 249 total and we have {foundGeoCount}");
 
             Print($"({validGeoCodes.Count} / 249) alpha-2 geo codes have been parsed.");
 
-            Print("Please enter your 2 digit geo code. eg.) US\n\tYou may also just hit enter and we will default your geo code to US");
-            string userInputGeoCode = Console.ReadLine();
+            Print(
+                "Please enter your 2 digit geo code. eg.) US\n\tYou may also just hit enter and we will default your geo code to US");
+            var userInputGeoCode = Console.ReadLine();
             Print($"You selected {userInputGeoCode} as your geo code.");
 
-            bool bMatchFound = false;
-            if (validGeoCodes.Any(geoCode => userInputGeoCode.ToLower() == geoCode.ToLower()))
-                bMatchFound = true;
+            var bMatchFound = validGeoCodes.Any(geoCode => userInputGeoCode?.ToLower() == geoCode.ToLower());
 
             /* Default to United States on invalid geo code.*/
             if (!bMatchFound)
             {
-                if (userInputGeoCode == "")
-                {
-                   Print("You did not select a geo-code. Defaulted to United States (US)");
-                } else {
-                   Print("Your geo code was invalid.\n\tYour search has been defaulted to the United States");
-                }
+                Print(
+                    userInputGeoCode == string.Empty
+                        ? "You did not select a geo-code. Defaulted to United States (US)"
+                        : "Your geo code was invalid.\n\tYour search has been defaulted to the United States");
+
                 userInputGeoCode = "US";
             }
-            
+
             driver.Url = $"https://trends.google.com/trends/?geo={userInputGeoCode}";
 
-            By topicSelector = By.ClassName("fe-explore-example-legend-text");
+            var topicSelector = By.ClassName("fe-explore-example-legend-text");
             IReadOnlyCollection<IWebElement> popularTopics = driver.FindElements(topicSelector);
 
             Print($"[{userInputGeoCode}] #1 Trending is {popularTopics.ElementAt(0).Text}");
             Print($"[{userInputGeoCode}] #2 Trending is {popularTopics.ElementAt(1).Text}");
 
             Print("Please enter a custom search parameter. eg.) Music");
-            string userInputCustomSearch = Console.ReadLine();
+            var userInputCustomSearch = Console.ReadLine();
 
-            By inputSelector = By.XPath("//input[@placeholder=\"Enter a search term or a topic\"]");
-            IWebElement searchInput = driver.FindElement(inputSelector);
+            var inputSelector = By.XPath("//input[@placeholder=\"Enter a search term or a topic\"]");
+            var searchInput = driver.FindElement(inputSelector);
             searchInput.Click();
             searchInput.SendKeys($"{userInputCustomSearch}{Keys.Return}");
 
@@ -114,6 +116,20 @@ namespace DotNetConsoleAppTest
             // Stop console from closing automatically
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// The print utility.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        private static void Print(object message)
+        {
+            BringConsoleToFront();
+            Console.WriteLine(string.Concat(Enumerable.Repeat("-", 96)));
+            Console.WriteLine(message.ToString());
+            Console.WriteLine(string.Concat(Enumerable.Repeat("-", 96)));
         }
     }
 }
